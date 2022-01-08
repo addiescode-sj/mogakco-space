162fb3e2de29536839143df722745a3ef4a22406
youngmi <wjdym14@gmail.com>
2022-01-09 01:04:36 +0900

    FEAT add aboutme in page

--- src/pages/AboutMe/components/Cards/Cards.Hero.tsx
+++ src/pages/AboutMe/components/Cards/Cards.Hero.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function HeroCard() {
     return (
         <div>
-            <div></div>
+            <p>안녕하세요!</p>
+            <p>프론트엔드 개발자</p>
+            <p><span>이애디</span> 입니다.</p>
         </div>
     )
 }
