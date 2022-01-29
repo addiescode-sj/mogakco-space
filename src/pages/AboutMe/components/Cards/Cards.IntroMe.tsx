b233443f9fddbed47aa59b64414ffe3d58dfb710
addiescode-sj <addiescode@gmail.com>
2022-01-28 23:51:53 -0500

    feat: added ui text interaction on desktop screen

--- src/pages/AboutMe/components/Cards/Cards.IntroMe.tsx
+++ src/pages/AboutMe/components/Cards/Cards.IntroMe.tsx
@@ -1,19 +1,30 @@
+import TextWithEffectScroll from '@components/TextWithEffect'
+import { useScreenContext } from '@hooks/useScreenContext'
+import { introText } from '@pages/AboutMe/data'
 import React from 'react'
 import * as S from './Cards.style'
 
 function IntroMe() {
+  const { isSmall } = useScreenContext()
+
+  if (!isSmall) {
+    return (
+      <S.IntroduceMeWrap>
+        {introText.split('\n').map((text) => (
+          <TextWithEffectScroll size="sm" weight={'bold'}>
+            {text}
+          </TextWithEffectScroll>
+        ))}
+      </S.IntroduceMeWrap>
+    )
+  }
   return (
     <S.IntroduceMeWrap>
       <S.Title>저는 __하는 개발자입니다.</S.Title>
       <br />
-      <S.Description>
-        <p>
-          HCI, UXUI를 수학하고 관련 분야에서 일해본 경험이 있어
-          <br /> 사용자 경험 개선에 대해 함께 고민하고, <br /> 어떻게 하면 효과적으로 매 Sprint마다
-          MVP를 만들 수 있는지 <br />
-          생각할 수 있는 역량을 가진 개발자.
-        </p>
-      </S.Description>
+      <S.DevelopScr>
+        <pre>{introText}</pre>
+      </S.DevelopScr>
     </S.IntroduceMeWrap>
   )
 }
